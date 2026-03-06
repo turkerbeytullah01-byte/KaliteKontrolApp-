49456616e2554a816d5a81951298657d453d2986
turkerbeytullah01-byte <turkerbeytullah01@gmail.com>
2026-03-06 22:17:23 +0300

    Create MeasurementControl.cs

---
+++ KaliteKontrolApp/KaliteKontrolApp/Forms/MeasurementControl.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+
+namespace KaliteKontrolApp.Forms
+{
+    public partial class MeasurementControl : UserControl
+    {
+        public MeasurementControl()
+        {
+            BackColor = ThemeColors.Background;
+            Dock = DockStyle.Fill;
+        }
+    }
+}
