b7eb983b5df8cf917783b67cf9f3affe5410b34e
turkerbeytullah01-byte <turkerbeytullah01@gmail.com>
2026-03-06 22:01:42 +0300

    Create ThemeColors.cs

---
+++ KaliteKontrolApp/KaliteKontrolApp/Utils/ThemeColors.cs
@@ -0,0 +1,22 @@
+using System.Drawing;
+
+namespace KaliteKontrolApp
+{
+    public static class ThemeColors
+    {
+        public static Color Background = Color.FromArgb(245, 246, 250);
+        public static Color Primary = Color.FromArgb(67, 97, 238);
+        public static Color Secondary = Color.FromArgb(114, 9, 183);
+        public static Color Success = Color.FromArgb(46, 213, 115);
+        public static Color Error = Color.FromArgb(255, 71, 87);
+        public static Color Warning = Color.FromArgb(255, 165, 2);
+        public static Color Info = Color.FromArgb(54, 162, 235);
+        
+        public static Color TextPrimary = Color.FromArgb(45, 52, 70);
+        public static Color TextSecondary = Color.FromArgb(108, 117, 125);
+        public static Color TextMuted = Color.FromArgb(173, 181, 189);
+        
+        public static Color CardBackground = Color.White;
+        public static Color CardBorder = Color.FromArgb(222, 226, 230);
+    }
+}
