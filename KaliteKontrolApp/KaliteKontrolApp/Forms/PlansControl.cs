ba3312989527d6aabfb0d4ee672715eb41e5f690
turkerbeytullah01-byte <turkerbeytullah01@gmail.com>
2026-03-06 22:17:00 +0300

    Create PlansControl.cs

---
+++ KaliteKontrolApp/KaliteKontrolApp/Forms/PlansControl.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+
+namespace KaliteKontrolApp.Forms
+{
+    public partial class PlansControl : UserControl
+    {
+        public PlansControl()
+        {
+            BackColor = ThemeColors.Background;
+            Dock = DockStyle.Fill;
+        }
+    }
+}
