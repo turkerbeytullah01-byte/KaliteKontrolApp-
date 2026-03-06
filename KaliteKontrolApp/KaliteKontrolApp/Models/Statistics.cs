f209bd4362c76b143a6de9dec700778a9cc74910
turkerbeytullah01-byte <turkerbeytullah01@gmail.com>
2026-03-06 21:46:16 +0300

    Create Statistics.cs

---
+++ KaliteKontrolApp/KaliteKontrolApp/Models/Statistics.cs
@@ -0,0 +1,11 @@
+namespace KaliteKontrolApp.Models
+{
+    public class Statistics
+    {
+        public int TotalPlans { get; set; }
+        public int TotalMeasurements { get; set; }
+        public int OkProducts { get; set; }
+        public int NokProducts { get; set; }
+        public int ConditionalProducts { get; set; }
+    }
+}
