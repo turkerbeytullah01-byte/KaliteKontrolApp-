47394937e6cd12618aa821ca96d0f43d57d3f146
turkerbeytullah01-byte <turkerbeytullah01@gmail.com>
2026-03-06 22:18:07 +0300

    Create SettingsControl.cs

---
+++ KaliteKontrolApp/KaliteKontrolApp/Forms/SettingsControl.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+
+namespace KaliteKontrolApp.Forms
+{
+    public partial class SettingsControl : UserControl
+    {
+        public SettingsControl()
+        {
+            BackColor = ThemeColors.Background;
+            Dock = DockStyle.Fill;
+        }
+    }
+}
