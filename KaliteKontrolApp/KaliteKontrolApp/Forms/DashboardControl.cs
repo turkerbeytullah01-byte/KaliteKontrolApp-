28b1573dd76d2d21207a44e1959910e016b98950
turkerbeytullah01-byte <turkerbeytullah01@gmail.com>
2026-03-06 22:16:32 +0300

    Create DashboardControl.cs

---
+++ KaliteKontrolApp/KaliteKontrolApp/Forms/DashboardControl.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+
+namespace KaliteKontrolApp.Forms
+{
+    public partial class DashboardControl : UserControl
+    {
+        public DashboardControl()
+        {
+            BackColor = ThemeColors.Background;
+            Dock = DockStyle.Fill;
+        }
+    }
+}
