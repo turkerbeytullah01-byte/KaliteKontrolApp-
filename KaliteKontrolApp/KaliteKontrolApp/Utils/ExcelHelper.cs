33f109aaac02e5b16f7b88d818a691f263d4f1a3
turkerbeytullah01-byte <turkerbeytullah01@gmail.com>
2026-03-06 22:03:40 +0300

    Create ExcelHelper.cs

---
+++ KaliteKontrolApp/KaliteKontrolApp/Utils/ExcelHelper.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace KaliteKontrolApp.Utils
+{
+    public static class ExcelHelper
+    {
+        public static void ExportPlansToExcel(List<object> plans, string fileName)
+        {
+            // TODO: Excel export implementasyonu
+        }
+
+        public static void ExportMeasurementsToExcel(List<object> measurements, string fileName)
+        {
+            // TODO: Excel export implementasyonu
+        }
+    }
+}
