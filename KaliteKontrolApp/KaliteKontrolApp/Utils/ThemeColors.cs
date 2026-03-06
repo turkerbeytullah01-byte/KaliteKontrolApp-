836cb0413f2f023b1e5b1a0d6206c122c8fc3f18
turkerbeytullah01-byte <turkerbeytullah01@gmail.com>
2026-03-06 22:14:32 +0300

    Update ThemeColors.cs

--- KaliteKontrolApp/KaliteKontrolApp/Utils/ThemeColors.cs
+++ KaliteKontrolApp/KaliteKontrolApp/Utils/ThemeColors.cs
@@ -4,19 +4,33 @@ namespace KaliteKontrolApp
 {
     public static class ThemeColors
     {
-        public static Color Background = Color.FromArgb(245, 246, 250);
+        // Ana Renkler
         public static Color Primary = Color.FromArgb(67, 97, 238);
+        public static Color Primary50 = Color.FromArgb(239, 242, 255);
         public static Color Secondary = Color.FromArgb(114, 9, 183);
+        
+        // Durum Renkleri
         public static Color Success = Color.FromArgb(46, 213, 115);
         public static Color Error = Color.FromArgb(255, 71, 87);
         public static Color Warning = Color.FromArgb(255, 165, 2);
         public static Color Info = Color.FromArgb(54, 162, 235);
         
+        // Arka Plan Renkleri
+        public static Color Background = Color.FromArgb(245, 246, 250);
+        public static Color CardBackground = Color.White;
+        
+        // Metin Renkleri
         public static Color TextPrimary = Color.FromArgb(45, 52, 70);
         public static Color TextSecondary = Color.FromArgb(108, 117, 125);
         public static Color TextMuted = Color.FromArgb(173, 181, 189);
+        public static Color TextOnPrimary = Color.White;
         
-        public static Color CardBackground = Color.White;
-        public static Color CardBorder = Color.FromArgb(222, 226, 230);
+        // Kenarlık ve Ayrım Renkleri
+        public static Color Border = Color.FromArgb(222, 226, 230);
+        public static Color BorderLight = Color.FromArgb(233, 236, 239);
+        
+        // Sidebar Renkleri
+        public static Color SidebarText = Color.FromArgb(173, 181, 189);
+        public static Color SidebarHover = Color.FromArgb(67, 97, 238);
     }
 }
